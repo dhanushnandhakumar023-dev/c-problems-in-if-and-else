4c65df960da0470e38a3d3865bb0bcd22ffd2ff5
dhanushnandhakumar023-dev <dhanushnandhakumar023@gmail.com>
2026-02-12 09:47:23 +0530

    Create Three Digit Number.c

---
+++ Three Digit Number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if  (num<=999 && num>=100)
+    {
+        printf("Three-digit number ");
+    }
+    else
+    {
+        printf("This is Not Three-digit number ");
+    }
+    return 0;
+}
